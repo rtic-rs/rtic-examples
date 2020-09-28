c8d55783c20c07e06ac63e3ee76cc5748298e4f5
Henrik Tjäder <henrik@tjaders.com>
2020-09-28 11:06:11 +0000

    Remove Travis

--- ci/install.sh
+++
@@ -1,7 +0,0 @@
-set -euxo pipefail
-
-main() {
-    rustup target add thumbv6m-none-eabi thumbv7m-none-eabi thumbv7em-none-eabihf
-}
-
-main
--- ci/script.sh
+++
@@ -1,8 +0,0 @@
-set -euxo pipefail
-
-main() {
-    # Prune for crates and check them
-    find . -type f -name Cargo.toml -execdir cargo build --release ';'
-}
-
-main
