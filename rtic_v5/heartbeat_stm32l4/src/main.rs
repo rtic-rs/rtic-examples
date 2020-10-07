677a52fbf67b341c646e7d21fa3742bb71e69707
Domenico Andreoli <domenico.andreoli@linux.com>
2020-10-07 23:47:53 +0000

    Implement the heart beat

--- rtic_v5/heartbeat_stm32l4/src/main.rs
+++ rtic_v5/heartbeat_stm32l4/src/main.rs
@@ -4,14 +4,21 @@
 #![no_std]
 
 extern crate panic_semihosting;
-use embedded_hal::digital::v2::OutputPin;
+
+use heapless::Vec;
+use heapless::consts::*;
+
 use rtic::app;
 use rtic::cyccnt::U32Ext;
 
 use stm32l4xx_hal::gpio::{gpiob::PB3, Output, PushPull, State};
 use stm32l4xx_hal::prelude::*;
 
-const PERIOD: u32 = 100_000_000;
+const BEATS_PER_MIN: u32 = 60;
+const CLK_SPEED_MHZ: u32 = 72;
+
+// Cycles per thousandth of beat
+const MILLI_BEAT: u32 = CLK_SPEED_MHZ * 60_000 / BEATS_PER_MIN;
 
 // We need to pass monotonic = rtic::cyccnt::CYCCNT to use schedule feature fo RTIC
 #[app(device = stm32l4xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
@@ -20,6 +27,7 @@ const APP: () = {
     // `LateResources` struct in init
     struct Resources {
         led: PB3<Output<PushPull>>,
+        intervals: Vec<u32, U6>,
     }
 
     #[init(schedule = [blinker])]
@@ -33,7 +41,7 @@ const APP: () = {
         let mut rcc = cx.device.RCC.constrain();
         let _clocks = rcc
             .cfgr
-            .sysclk(72.mhz())
+            .sysclk(CLK_SPEED_MHZ.mhz())
             .freeze(&mut flash.acr);
 
         // Setup LED
@@ -42,25 +50,35 @@ const APP: () = {
             .pb3
             .into_push_pull_output_with_state(&mut gpiob.moder, &mut gpiob.otyper, State::Low);
 
-        // Schedule the blinking task
-        cx.schedule.blinker(cx.start + PERIOD.cycles()).unwrap();
+        // Simple heart beat LED on/off sequence
+        let mut intervals: Vec<u32, U6> = Vec::new();
+        intervals.push(MILLI_BEAT * 30).unwrap();  // P Wave
+        intervals.push(MILLI_BEAT * 40).unwrap();  // PR Segment
+        intervals.push(MILLI_BEAT * 120).unwrap(); // QRS Complex
+        intervals.push(MILLI_BEAT * 30).unwrap();  // ST Segment
+        intervals.push(MILLI_BEAT * 60).unwrap();  // T Wave
+        intervals.push(MILLI_BEAT * 720).unwrap(); // Rest
 
-        init::LateResources { led: led }
+        // Schedule the blinking task
+        cx.schedule.blinker(cx.start, 0).unwrap();
+
+        init::LateResources { led, intervals }
     }
 
-    #[task(resources = [led], schedule = [blinker])]
-    fn blinker(cx: blinker::Context) {
-        // Use the safe local `static mut` of RTIC
-        static mut LED_STATE: bool = false;
+    #[task(schedule = [blinker], resources = [led, &intervals])]
+    fn blinker(cx: blinker::Context, state: usize) {
+        let led = cx.resources.led;
+        let intervals = cx.resources.intervals;
+        let duration = intervals[state].cycles();
+        let next_state = (state + 1) % intervals.len();
 
-        if *LED_STATE {
-            cx.resources.led.set_high().unwrap();
-            *LED_STATE = false;
+        if state % 2 == 0 {
+            led.set_high().unwrap();
         } else {
-            cx.resources.led.set_low().unwrap();
-            *LED_STATE = true;
+            led.set_low().unwrap();
         }
-        cx.schedule.blinker(cx.scheduled + PERIOD.cycles()).unwrap();
+
+        cx.schedule.blinker(cx.scheduled + duration, next_state).unwrap();
     }
 
     extern "C" {
