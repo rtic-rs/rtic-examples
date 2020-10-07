77a6ea9814767303099a01b4df0bf242ab0e26fd
Domenico Andreoli <domenico.andreoli@linux.com>
2020-10-07 00:19:25 +0000

    Port to stm32l4xx

--- rtic_v5/heartbeat_stm32l4/src/main.rs
+++ rtic_v5/heartbeat_stm32l4/src/main.rs
@@ -7,18 +7,19 @@ extern crate panic_semihosting;
 use embedded_hal::digital::v2::OutputPin;
 use rtic::app;
 use rtic::cyccnt::U32Ext;
-use stm32f1xx_hal::gpio::{gpioc::PC13, Output, PushPull, State};
-use stm32f1xx_hal::prelude::*;
+
+use stm32l4xx_hal::gpio::{gpiob::PB3, Output, PushPull, State};
+use stm32l4xx_hal::prelude::*;
 
 const PERIOD: u32 = 100_000_000;
 
 // We need to pass monotonic = rtic::cyccnt::CYCCNT to use schedule feature fo RTIC
-#[app(device = stm32f1xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
+#[app(device = stm32l4xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     // Global resources (global variables) are defined here and initialized with the 
     // `LateResources` struct in init
     struct Resources {
-        led: PC13<Output<PushPull>>,
+        led: PB3<Output<PushPull>>,
     }
 
     #[init(schedule = [blinker])]
@@ -27,25 +28,19 @@ const APP: () = {
         let mut core = cx.core;
         core.DWT.enable_cycle_counter();
 
-        let device: stm32f1xx_hal::stm32::Peripherals = cx.device;
-
         // Setup clocks
-        let mut flash = device.FLASH.constrain();
-        let mut rcc = device.RCC.constrain();
-        let mut _afio = device.AFIO.constrain(&mut rcc.apb2);
+        let mut flash = cx.device.FLASH.constrain();
+        let mut rcc = cx.device.RCC.constrain();
         let _clocks = rcc
             .cfgr
-            .use_hse(8.mhz())
             .sysclk(72.mhz())
-            .pclk1(36.mhz())
             .freeze(&mut flash.acr);
 
         // Setup LED
-        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
-        let mut led = gpioc
-            .pc13
-            .into_push_pull_output_with_state(&mut gpioc.crh, State::Low);
-        led.set_low().unwrap();
+        let mut gpiob = cx.device.GPIOB.split(&mut rcc.ahb2);
+        let led = gpiob
+            .pb3
+            .into_push_pull_output_with_state(&mut gpiob.moder, &mut gpiob.otyper, State::Low);
 
         // Schedule the blinking task
         cx.schedule.blinker(cx.start + PERIOD.cycles()).unwrap();
