b35dec9c1cb2270adbcfc39a278574e67ef4b53a
Ralph Ursprung <ralph.ursprung@gmail.com>
2022-12-08 14:37:57 +0100

    automate `dependabot.yml` updating, add check to CI for it
    to automate the process, a new script has been created to make life
    easier in the future.
    this script has also been included in the CI build and `git diff
    --quiet` is used to check whether there are any modifications in the
    local working directory (returns a failure status if any modifications
    are present). this way it's not possible to forget adding a new example
    to the dependabot config.
    `git status` and `git diff` are also run in the workflow to make it more
    obvious what the reason for the failure is in case there's a difference
    (was very helpful for me now to debug the workflow because i learnt that
    i had to sort the results of `find` as the order would otherwise be
    arbitrary and on the CI it'd be different than locally, thus resulting
    in a diff...).
    the `LC_ALL=C` is required to ensure that the sorting is done the same
    on all machines.

---
+++ update_dependabot_config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+cat <<EOF > .github/dependabot.yml
+# file automatically generated using update_dependabot.sh, re-run the script to update this file!
+version: 2
+updates:
+  - package-ecosystem: "github-actions"
+    directory: "/"
+    schedule:
+      interval: "weekly"
+EOF
+
+export LC_ALL=C
+for fld in $(find . -type f -name Cargo.toml | sort | sed 's#/Cargo.toml##' | sed 's#./#/#') ; do
+    cat <<EOF >> .github/dependabot.yml
+  - package-ecosystem: "cargo"
+    directory: "${fld}"
+    schedule:
+      interval: "weekly"
+EOF
+done
